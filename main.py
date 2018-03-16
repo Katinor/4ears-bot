8e972fd041fa21b21a07f2bd04f7cd6e1a383a22
Katinor <katinor@4ears.net>
2018-03-16 17:43:24 +0900

    fix bugs.

--- main.py
+++ main.py
@@ -195,13 +195,13 @@ async def dialog_how(msg,user):
 			em = discord.Embed(title=user_list[0].name+"#"+str(user_list[0].discriminator)+" 의 프로필!",description="Lv."+str(level_stat)+" ( "+str(cur_exp)+" / "+str(req_exp)+" )\n"+exp_bar+exp_perc_str, colour=discord.Colour.blue())
 		else : 
 			em = discord.Embed(title=user_list[0].name+"#"+str(user_list[0].discriminator)+" 의 프로필!",description="Lv."+str(level_stat)+" ( "+str(cur_exp)+" )\n■■■■■■■■■■(최대레벨)", colour=discord.Colour.blue())
-		ifuser_list[0].avatar_url:	em.set_thumbnail(url=user_list[0].avatar_url)
+		if user_list[0].avatar_url:	em.set_thumbnail(url=user_list[0].avatar_url)
 		else : em.set_thumbnail(url="https://i.imgur.com/pg7K8cQ.png")
 		em.add_field(name="돈", value=str(cash_stat), inline=True)
 		em.add_field(name="호감도", value=trg_user.love_short(), inline=True)
 		if level_stat == 100: em.add_field(name="누적 경험치",value=str(trg_user.exp),inline=False)
 		else : em.add_field(name="누적 경험치",value=str(trg_user.exp)+" / "+str(quadra_user_module.REQ_EXP[level_stat]),inline=False)
-		await bot.send_message(msg.channel,mention_user(trg_user.user_id)+",\n"+trg_user.love_text(),embed = em)
+		await bot.send_message(msg.channel,mention_user(user.user_id)+", "+user_list[0].name+"#"+str(user_list[0].discriminator)+"에 대해서 묻는거지?",embed = em)
 	elif target[0] in quadra_search_vocab.dis_list: 
 		text=random.choice(quadra_dialog_list.dialog_dis_how)
 		user.mody(love = 2, love_time = True, exp = 5, exp_time = True)
